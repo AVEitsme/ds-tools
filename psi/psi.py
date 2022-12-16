1bc4c9f2666662dfe3c6e395174fa7ecd4b5f6aa
Artem Egorov <aveitsme@gmail.com>
2022-12-17 01:43:51 +0700

    Output format fix

--- psi/psi.py
+++ psi/psi.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from typing import Dict
 
 
 class PSI:
@@ -11,22 +11,20 @@ class PSI:
         assert all(self.actual_sample.columns == self.expected_sample.columns)
         self.bin_counts = bin_counts
     
-    def psi_sum(self) -> float:
-        """Sum of all PSI scores in dataset."""
-        psi_scores = np.empty(self.actual_sample.columns.shape[0])
-        for index, column in enumerate(self.actual_sample.columns):
-            psi_scores[index] = self.eval_psi(
+    def eval_psi(self) -> Dict[str, float]:
+        """Return column: PSI score for all features."""
+        psi_scores = {}
+        for column in self.actual_sample.columns:
+            psi_scores[column] =self.psi(
                 actual=self.actual_sample[column].to_numpy(),
                 expected=self.expected_sample[column].to_numpy()
-            )
-        return psi_scores.sum() / len(psi_scores)
+            ) 
+        return psi_scores
 
-    def eval_psi(self, actual: np.ndarray, expected: np.ndarray) -> float:
-        """Eval PSI."""
+    def psi(self, actual: np.ndarray, expected: np.ndarray) -> float:
+        """Eval PSI for single feature."""
         actual.sort()
         expected.sort()
         actual_percentage = np.array(np.histogram(actual, self.bin_counts)[0]) / len(actual) + 1
         expected_percentage = np.array(np.histogram(expected, self.bin_counts)[0]) / len(expected) + 1
         return ((actual_percentage - expected_percentage) * np.log(actual_percentage / expected_percentage)).sum()
-
-

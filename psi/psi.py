1c83ae66dc41abb09e4208507887a5a7605f172e
Artem Egorov <aveitsme@gmail.com>
2022-12-19 23:17:31 +0700

    PSI test rebuilded

--- psi/psi.py
+++ psi/psi.py
@@ -3,28 +3,42 @@ import pandas as pd
 from typing import Dict
 
 
+def scale_range(source: np.ndarray, _min: float, _max: float) -> np.ndarray:
+    source -= source.min()
+    source /= source.max() / (_max - _min)
+    source += _min
+    return source
+
+
 class PSI:
-    """Represents class to eval Population Stability Index"""
-    def __init__(self, actual_sample: pd.DataFrame, expected_sample: pd.DataFrame, bin_counts: int):
-        self.actual_sample = actual_sample
-        self.expected_sample = expected_sample
-        assert all(self.actual_sample.columns == self.expected_sample.columns)
-        self.bin_counts = bin_counts
+    """Represents class to eval Population Stability Index."""
+    def __init__(self, initial_sample: pd.DataFrame, new_sample: pd.DataFrame):
+        assert all(initial_sample.columns == new_sample.columns)
+        self.initial_sample = initial_sample
+        self.new_sample = new_sample
     
-    def eval_psi(self) -> Dict[str, float]:
+    def eval_psi(self, bin_counts, epsilon) -> Dict[str, float]:
         """Return column: PSI score for all features."""
         psi_scores = {}
-        for column in self.actual_sample.columns:
+        for column in self.initial_sample.columns:
             psi_scores[column] =self.psi(
-                actual=self.actual_sample[column].to_numpy(),
-                expected=self.expected_sample[column].to_numpy()
+                initial=self.initial_sample[column].to_numpy(),
+                new=self.new_sample[column].to_numpy(),
+                bin_counts=bin_counts,
+                epsilon=epsilon
             ) 
         return psi_scores
 
-    def psi(self, actual: np.ndarray, expected: np.ndarray) -> float:
+    @staticmethod
+    def psi(initial: np.ndarray, new: np.ndarray, bin_counts: int, epsilon: float) -> float:
         """Eval PSI for single feature."""
-        actual.sort()
-        expected.sort()
-        actual_percentage = np.array(np.histogram(actual, self.bin_counts)[0]) / len(actual) + 1
-        expected_percentage = np.array(np.histogram(expected, self.bin_counts)[0]) / len(expected) + 1
-        return ((actual_percentage - expected_percentage) * np.log(actual_percentage / expected_percentage)).sum()
+        bins = scale_range(
+            source=np.arange(0, bin_counts + 1, dtype=float),
+            _min=initial.min(),
+            _max=initial.max()
+        )
+
+        initial_percentage = np.array(np.histogram(initial, bins)[0]) / len(initial) + epsilon
+        new_percentage = np.array(np.histogram(new, bins)[0]) / len(new) + epsilon
+        return ((initial_percentage - new_percentage) * np.log(initial_percentage / new_percentage)).sum()
+

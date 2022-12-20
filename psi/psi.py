c27ecd5b497bab6841f76ea5df272cb7ce8d1f95
Artem Egorov <aveitsme@gmail.com>
2022-12-20 23:49:00 +0700

    Debugged

--- psi/psi.py
+++ psi/psi.py
@@ -4,6 +4,7 @@ from typing import Dict
 
 
 def scale_range(source: np.ndarray, _min: float, _max: float) -> np.ndarray:
+    """Scale range according min & max."""
     source -= source.min()
     source /= source.max() / (_max - _min)
     source += _min
@@ -18,10 +19,10 @@ class PSI:
         self.new_sample = new_sample
     
     def eval_psi(self, bin_counts, epsilon) -> Dict[str, float]:
-        """Return column: PSI score for all features."""
+        """Return {column: PSI score} for all features."""
         psi_scores = {}
         for column in self.initial_sample.columns:
-            psi_scores[column] =self.psi(
+            psi_scores[column] = self.psi(
                 initial=self.initial_sample[column].to_numpy(),
                 new=self.new_sample[column].to_numpy(),
                 bin_counts=bin_counts,
@@ -36,11 +37,11 @@ class PSI:
         new.sort()
         bins = scale_range(
             source=np.arange(0, bin_counts + 1, dtype=float),
-            _min=initial.min(),
-            _max=initial.max()
+            _min=min(initial.min(), new.min()),
+            _max=max(initial.max(), new.max())
         )
         initial_percentage = np.array(np.histogram(initial, bins)[0]) / len(initial)
         new_percentage = np.array(np.histogram(new, bins)[0]) / len(new)
         initial_percentage[initial_percentage == 0] = epsilon
         new_percentage[new_percentage == 0] = epsilon
-        return ((initial_percentage - new_percentage) * np.log(initial_percentage / new_percentage)).sum()
+        return ((initial_percentage - new_percentage) * (np.log(initial_percentage) - np.log(new_percentage))).sum()
